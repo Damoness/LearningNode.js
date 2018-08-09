74655fae084c0fbd0dc95893a2f02002ec2be602
Damoness-Mini <qjwdcool@qq.com>
2018-08-09 17:14:50 +0800

    Express使用

--- MongoDB/ExpressTest.js
+++ MongoDB/ExpressTest.js
@@ -2,14 +2,59 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world1');
+
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'mongodb';
+let MongoClient = require("mongodb").MongoClient;
+
+
+let engines = require('consolidate');
+app.engine("html",engines.nunjucks);
+app.set("view engine","html");
+app.set("views",__dirname + "/views");
+
+
+
+MongoClient.connect(url,(err,client)=>{
+
+    const db = client.db(dbName);
+
+    app.get('/v1/home/list', function(req, res){
+
+        db.collection('newslist').find().limit(10).toArray((err,result)=>{
+
+            res.send(result);
+
+        });
+
+    });
+
+
 });
+
+
+app.get('/:name',(req,res)=>{
+
+    let name = req.params.name;
+    let query = req.query;
+
+    res.send({
+       name:name,
+       query:query,
+    });
+
+});
+
 
 
 let server = app.listen(3000,()=>{
 
-        let port = server.address().port;
-        console.log('listening on port %s',port);
+    let port = server.address().port;
+    console.log('listening on port %s',port);
 
-    });
+});
+
+
